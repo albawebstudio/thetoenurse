5028b95f53fc48e4a09669f59c6024c8f83986f5
Andrew Alba <andrew.alba@gmail.com>
2024-07-30 15:36:25 -0500

    ✏️ fix(useTestimonials.ts): correct spelling of name from 'Tricia' to 'Trisha' in testimonials content for accuracy and consistency

--- composables/useTestimonials.ts
+++ composables/useTestimonials.ts
@@ -14,7 +14,7 @@ export function useTestimonials() {
         {
             "id": 1,
             "rating": "4.0",
-            "content": "I was a little nervous about getting my feet taken care of, but Tricia made it such a breeze! As a young guy in my 20s, I wasn't sure what to expect, but Tricia was so professional and friendly that I felt right at home. She even met me at a neutral location, which was super convenient for me. The foot care service itself was amazing - it was so relaxing and refreshing. I left feeling like I was walking on clouds! I'm definitely planning on scheduling another appointment with Tricia soon. If you're looking for a foot care specialist who knows how to make you feel comfortable and taken care of, Tricia is the way to go!",
+            "content": "I was a little nervous about getting my feet taken care of, but Trisha made it such a breeze! As a young guy in my 20s, I wasn't sure what to expect, but Trisha was so professional and friendly that I felt right at home. She even met me at a neutral location, which was super convenient for me. The foot care service itself was amazing - it was so relaxing and refreshing. I left feeling like I was walking on clouds! I'm definitely planning on scheduling another appointment with Trisha soon. If you're looking for a foot care specialist who knows how to make you feel comfortable and taken care of, Trisha is the way to go!",
             "author": "Rafael Cormier",
             "image": "michelle-and-andrew.jpg",
             "location": "Plainfield, MN"
@@ -22,7 +22,7 @@ export function useTestimonials() {
         {
             "id": 2,
             "rating": "5.0",
-            "content": "I am extremely grateful to Tricia for the exceptional foot care services she provided to my father. He was unable to take care of his own feet due to his medical condition, but Tricia was patient and understanding throughout the entire process. She went above and beyond to ensure that my father was comfortable and well-cared for, and her expertise and professionalism were evident in every aspect of her work. I highly recommend Tricia to anyone in need of foot care services. Thank you, Tricia, for everything you do!",
+            "content": "I am extremely grateful to Trisha for the exceptional foot care services she provided to my father. He was unable to take care of his own feet due to his medical condition, but Trisha was patient and understanding throughout the entire process. She went above and beyond to ensure that my father was comfortable and well-cared for, and her expertise and professionalism were evident in every aspect of her work. I highly recommend Trisha to anyone in need of foot care services. Thank you, Trisha, for everything you do!",
             "author": "Kailey Reinger",
             "image": "michelle-and-andrew.jpg",
             "location": "Grand Junction, MN"
@@ -30,7 +30,7 @@ export function useTestimonials() {
         {
             "id": 3,
             "rating": "5.0",
-            "content": "I never thought getting my feet taken care of could be so much fun! Tricia is the best foot care specialist out there! As a middle-aged guy, I was a little nervous about getting my feet touched, but Tricia made me feel so comfortable and at ease. I had some concerns about a possible fungus on my toe, but Tricia took care of it painlessly and quickly. We had such a great conversation during the appointment that I almost forgot I was getting my feet taken care of! I'm so grateful for Tricia's foot care service and would recommend her to anyone who wants to have a good time while taking care of their feet!",
+            "content": "I never thought getting my feet taken care of could be so much fun! Trisha is the best foot care specialist out there! As a middle-aged guy, I was a little nervous about getting my feet touched, but Trisha made me feel so comfortable and at ease. I had some concerns about a possible fungus on my toe, but Trisha took care of it painlessly and quickly. We had such a great conversation during the appointment that I almost forgot I was getting my feet taken care of! I'm so grateful for Trisha's foot care service and would recommend her to anyone who wants to have a good time while taking care of their feet!",
             "author": "Nick Boyle",
             "image": "michelle-and-andrew.jpg",
             "location": "Evanston, MN"
@@ -39,7 +39,7 @@ export function useTestimonials() {
         {
             "id": 4,
             "rating": "5.0",
-            "content": "My grandma and I had the best time getting our feet taken care of by Tricia! She came to our home, which made the experience even more comfortable and convenient. We loved sharing the foot care service together - it was such a fun bonding experience for us! Tricia was so friendly and professional, and she really knows her stuff. We left feeling pampered and refreshed, and we're already planning on scheduling another appointment - maybe we'll even add more family members next time! If you're looking for a foot care specialist who can bring the fun to your home, Tricia is the one to call!",
+            "content": "My grandma and I had the best time getting our feet taken care of by Trisha! She came to our home, which made the experience even more comfortable and convenient. We loved sharing the foot care service together - it was such a fun bonding experience for us! Trisha was so friendly and professional, and she really knows her stuff. We left feeling pampered and refreshed, and we're already planning on scheduling another appointment - maybe we'll even add more family members next time! If you're looking for a foot care specialist who can bring the fun to your home, Trisha is the one to call!",
             "author": "Eloise Murphy",
             "image": "michelle-and-andrew.jpg",
             "location": "Middletown, MN"
